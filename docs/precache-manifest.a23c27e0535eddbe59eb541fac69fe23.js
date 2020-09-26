376a9ecd16547a16e0fc23b40e0d78fcbbfdd638
Ricardo <rbalamcm@gmail.com>
2020-09-26 00:16:16 -0500

    Doc agregado

---
+++ docs/precache-manifest.a23c27e0535eddbe59eb541fac69fe23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ae77f472e85ed942bdb28a8266cce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0551fc5419991cf759d6",
+    "url": "/static/css/main.6ba5160d.chunk.css"
+  },
+  {
+    "revision": "6d261711e8a9d737f9ac",
+    "url": "/static/js/2.472af925.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.472af925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0551fc5419991cf759d6",
+    "url": "/static/js/main.db0faa87.chunk.js"
+  },
+  {
+    "revision": "db84365a4efebe03af37",
+    "url": "/static/js/runtime-main.e6617295.js"
+  }
+]);
